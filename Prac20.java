fbfeac61b0c8e2cc295ceb89966c841fc7ff510b
komal131 <73995389+komal131@users.noreply.github.com>
2020-11-25 20:52:17 +0530

    Create Prac20.java

---
+++ Prac20.java
@@ -0,0 +1,15 @@
+import java.applet.Applet;
+
+import java.awt.Graphics;
+
+public class HelloWorldApplet extends Applet
+{
+
+ public void paint(Graphics g)
+{
+
+g.drawString("Hello World", 50, 50);
+
+}
+
+}
